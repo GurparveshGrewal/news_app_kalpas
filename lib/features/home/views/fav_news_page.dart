b3366927a7a6c10c75ee699e7837529f873b2415
Gurparvesh Singh <66719267+GurparveshGrewal@users.noreply.github.com>
2024-04-26 14:37:16 +0530

    feat: main screen's tab bar

---
+++ lib/features/home/views/fav_news_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class FavNewsPage extends StatelessWidget {
+  const FavNewsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Fav News'),
+      ),
+    );
+  }
+}
--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:news_app_kalpas/core/app_theme.dart';
+import 'package:news_app_kalpas/features/home/views/main_screen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Kalpas'),
-        ),
-      ),
+    return MaterialApp(
+      theme: AppThemeData().themeData,
+      home: const MainScreen(),
     );
   }
 }
