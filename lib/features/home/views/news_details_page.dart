922c8a5037e7fb47148ef09addf19203a5ced0b0
Gurparvesh Singh <66719267+GurparveshGrewal@users.noreply.github.com>
2024-04-28 00:10:57 +0530

    feat: added news details page

---
+++ lib/features/home/views/news_details_page.dart
@@ -0,0 +1,136 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:news_app_kalpas/core/utils/functions.dart';
+import 'package:news_app_kalpas/features/home/domain/entity/news_entity.dart';
+
+class NewsDetailsPage extends StatelessWidget {
+  final NewsDataEntity newsData;
+  const NewsDetailsPage({
+    super.key,
+    required this.newsData,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        automaticallyImplyLeading: false,
+        leadingWidth: MediaQuery.of(context).size.width * 0.5,
+        leading: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: GestureDetector(
+            onTap: Navigator.of(context).pop,
+            child: const Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox(
+                  width: 30,
+                  child: Icon(
+                    Icons.arrow_back_ios,
+                    color: Colors.black,
+                    size: 25,
+                  ),
+                ),
+                Text(
+                  'Back',
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: Scrollbar(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Stack(
+              children: [
+                SizedBox(
+                  height: height,
+                  width: width,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: SizedBox(
+                            height: height * 0.25,
+                            width: double.infinity,
+                            child: newsData.imageURL != null
+                                ? CachedNetworkImage(
+                                    key: UniqueKey(),
+                                    imageUrl: newsData.imageURL!,
+                                    placeholder: (context, url) => const Center(
+                                        child: CircularProgressIndicator()),
+                                    errorWidget: (context, url, error) =>
+                                        const Icon(Icons.error),
+                                    fit: BoxFit.cover,
+                                  )
+                                : Image.asset(
+                                    'assets/images/default_article_image.jpg',
+                                    fit: BoxFit.cover,
+                                  )),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Flexible(
+                        child: Text(
+                          newsData.title,
+                          style: const TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Flexible(
+                        child: FittedBox(
+                          child: Row(
+                            children: [
+                              Icon(
+                                size: 20,
+                                Icons.calendar_month_rounded,
+                                color: Theme.of(context).colorScheme.outline,
+                              ),
+                              Text(
+                                convertDateToReadable(newsData.publishedAt),
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 12,
+                                  color: Theme.of(context)
+                                      .colorScheme
+                                      .outline
+                                      .withOpacity(0.7),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Expanded(
+                          child: Text(
+                        newsData.description,
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      )),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/home/views/widgets/article_card.dart
+++ lib/features/home/views/widgets/article_card.dart
@@ -8,6 +8,7 @@ import 'package:news_app_kalpas/core/utils/functions.dart';
 import 'package:news_app_kalpas/core/utils/show_snackbar.dart';
 import 'package:news_app_kalpas/features/home/domain/entity/news_entity.dart';
 import 'package:news_app_kalpas/features/home/views/bloc/home_bloc.dart';
+import 'package:news_app_kalpas/features/home/views/news_details_page.dart';
 
 class NewsCard extends StatelessWidget {
   final NewsDataEntity newsData;
@@ -22,190 +23,197 @@ class NewsCard extends StatelessWidget {
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
-    return Dismissible(
-      key: UniqueKey(),
-      direction:
-          isFromFavsPage ? DismissDirection.none : DismissDirection.endToStart,
-      dragStartBehavior: DragStartBehavior.start,
-      confirmDismiss: (_) async {
-        final confirmed = await showDialog(
-          context: context,
-          builder: (ctx) {
-            return AlertDialog(
-              title: const Text(
-                'Add Article to Favorites',
-                style: TextStyle(
-                  fontWeight: FontWeight.w500,
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(MaterialPageRoute(
+            builder: (ctx) => NewsDetailsPage(newsData: newsData)));
+      },
+      child: Dismissible(
+        key: UniqueKey(),
+        direction: isFromFavsPage
+            ? DismissDirection.none
+            : DismissDirection.endToStart,
+        dragStartBehavior: DragStartBehavior.start,
+        confirmDismiss: (_) async {
+          final confirmed = await showDialog(
+            context: context,
+            builder: (ctx) {
+              return AlertDialog(
+                title: const Text(
+                  'Add Article to Favorites',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                  ),
                 ),
-              ),
-              content: const Text(
-                'Do you want to add this article to Favs?',
-                style: TextStyle(
-                  fontSize: 18,
+                content: const Text(
+                  'Do you want to add this article to Favs?',
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
                 ),
-              ),
-              actions: [
-                Row(
-                  children: [
-                    Expanded(
-                      child: TextButton(
-                        onPressed: () {
-                          Navigator.of(context).pop(false);
-                        },
-                        child: Text(
-                          'Cancel',
-                          style: TextStyle(
-                              fontSize: 18, color: Colors.grey.shade800),
+                actions: [
+                  Row(
+                    children: [
+                      Expanded(
+                        child: TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop(false);
+                          },
+                          child: Text(
+                            'Cancel',
+                            style: TextStyle(
+                                fontSize: 18, color: Colors.grey.shade800),
+                          ),
                         ),
                       ),
+                      Expanded(
+                        child: CustomButton(
+                          title: 'Add to Favs',
+                          onTap: () {
+                            context
+                                .read<HomeBloc>()
+                                .add(HomeAddNewsToFavsEvent(newsData));
+                            showSnackBar(context,
+                                'Successfully added article to the Favorites');
+                            Navigator.of(context).pop(false);
+                          },
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              );
+            },
+          );
+          return confirmed;
+        },
+        background: Container(
+          color: Colors.red.withOpacity(0.5),
+          child: Align(
+            alignment: Alignment.centerRight,
+            child: SizedBox(
+              height: double.infinity,
+              width: 100,
+              child: Padding(
+                padding: const EdgeInsets.only(right: 30.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Icon(
+                      color: Colors.red,
+                      CupertinoIcons.heart_fill,
+                      size: 30,
                     ),
-                    Expanded(
-                      child: CustomButton(
-                        title: 'Add to Favs',
-                        onTap: () {
-                          context
-                              .read<HomeBloc>()
-                              .add(HomeAddNewsToFavsEvent(newsData));
-                          showSnackBar(context,
-                              'Successfully added article to the Favorites');
-                          Navigator.of(context).pop(false);
-                        },
-                      ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      'Add to Favorite',
+                      style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                          color: Colors.black, fontWeight: FontWeight.bold),
                     )
                   ],
                 ),
-              ],
-            );
-          },
-        );
-        return confirmed;
-      },
-      background: Container(
-        color: Colors.red.withOpacity(0.5),
-        child: Align(
-          alignment: Alignment.centerRight,
-          child: SizedBox(
-            height: double.infinity,
-            width: 100,
-            child: Padding(
-              padding: const EdgeInsets.only(right: 30.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Icon(
-                    color: Colors.red,
-                    CupertinoIcons.heart_fill,
-                    size: 30,
-                  ),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  Text(
-                    'Add to Favorite',
-                    style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                        color: Colors.black, fontWeight: FontWeight.bold),
-                  )
-                ],
               ),
             ),
           ),
         ),
-      ),
-      child: Container(
-        height: height * 0.18,
-        width: double.infinity,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-          child: Row(
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: SizedBox(
-                    height: height * 0.12,
-                    width: width * 0.25,
-                    child: newsData.imageURL != null
-                        ? CachedNetworkImage(
-                            key: UniqueKey(),
-                            imageUrl: newsData.imageURL!,
-                            placeholder: (context, url) => const Center(
-                                child: CircularProgressIndicator()),
-                            errorWidget: (context, url, error) =>
-                                const Icon(Icons.error),
-                            fit: BoxFit.cover,
-                          )
-                        : Image.asset(
-                            'assets/images/default_article_image.jpg',
-                            fit: BoxFit.cover,
-                          )),
-              ),
-              const SizedBox(
-                width: 20,
-              ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 10),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        newsData.title,
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                        style: const TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 5,
-                      ),
-                      Expanded(
-                        child: Text(
+        child: Container(
+          height: height * 0.18,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Row(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: SizedBox(
+                      height: height * 0.12,
+                      width: width * 0.25,
+                      child: newsData.imageURL != null
+                          ? CachedNetworkImage(
+                              key: UniqueKey(),
+                              imageUrl: newsData.imageURL!,
+                              placeholder: (context, url) => const Center(
+                                  child: CircularProgressIndicator()),
+                              errorWidget: (context, url, error) =>
+                                  const Icon(Icons.error),
+                              fit: BoxFit.cover,
+                            )
+                          : Image.asset(
+                              'assets/images/default_article_image.jpg',
+                              fit: BoxFit.cover,
+                            )),
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
                           newsData.title,
-                          maxLines: 1,
+                          maxLines: 2,
                           overflow: TextOverflow.ellipsis,
                           style: const TextStyle(
-                            fontSize: 14,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
                           ),
                         ),
-                      ),
-                      const SizedBox(
-                        height: 5,
-                      ),
-                      Flexible(
-                        child: FittedBox(
-                          child: Row(
-                            children: [
-                              Icon(
-                                size: 20,
-                                Icons.calendar_month_rounded,
-                                color: Theme.of(context).colorScheme.outline,
-                              ),
-                              Text(
-                                convertDateToReadable(newsData.publishedAt),
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 12,
-                                  color: Theme.of(context)
-                                      .colorScheme
-                                      .outline
-                                      .withOpacity(0.7),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        Expanded(
+                          child: Text(
+                            newsData.title,
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: const TextStyle(
+                              fontSize: 14,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        Flexible(
+                          child: FittedBox(
+                            child: Row(
+                              children: [
+                                Icon(
+                                  size: 20,
+                                  Icons.calendar_month_rounded,
+                                  color: Theme.of(context).colorScheme.outline,
                                 ),
-                              ),
-                            ],
+                                Text(
+                                  convertDateToReadable(newsData.publishedAt),
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 12,
+                                    color: Theme.of(context)
+                                        .colorScheme
+                                        .outline
+                                        .withOpacity(0.7),
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
